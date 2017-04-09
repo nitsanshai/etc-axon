60013816e0055267c8ce9ded7e76d3ae4adbe6cc
Nitsan Shai <shai.nitsan@gmail.com>
2017-04-09 17:29:15 -0400

    got webpage to update on database read

--- js/game.js
+++ js/game.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
+
+    var headline_part_1 = '\
+        <div class="row container">\
+<div class="col s8 card-panel center-align white-text blue-grey darken-2">';
+    var headline_part_2 = '</div>\
+          <a class="col s4 l2 offset-l2 waves-effect waves-light btn amber darken-2"><i class="material-icons left">share</i>SHARE</a>\
+        </div>';
+
     setInterval(function() {
-        console.log($('#username').text());
         $.ajax({
             url: '/read-state',
             type: 'POST',
@@ -10,7 +17,16 @@ $(document).ready(function() {
             contentType:false,
             processData: false,
             success: function(state) {
-                console.log(state);
+                state = JSON.parse(state);
+                $('[data-headlines="private"]').empty();
+                for (var i in state['private_headlines']) {
+                    console.log(state['private_headlines'][i]);
+                    if (state['private_headlines'][i]) {
+                        var headline = state['private_headlines'][i];
+                        var elem = headline_part_1 + headline + headline_part_2;
+                        $('[data-headlines="private"]').append(elem);
+                    }
+                }
             }
         });
     }, 1000);
