0846429b64474c17b688dd1ca8b8dd97f568fbe4
Nitsan Shai <shai.nitsan@gmail.com>
2017-04-09 14:56:49 -0400

    initial commit with materialize

---
+++ main.py
@@ -0,0 +1,52 @@
+import os
+import time
+
+import jinja2
+import webapp2
+
+from google.appengine.api import taskqueue
+from google.appengine.ext import ndb
+
+
+JINJA_ENV = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+
+class Game(ndb.Model):
+    round = ndb.IntegerProperty()
+    cleared = ndb.BooleanProperty()
+    headlines = ndb.JsonProperty()
+
+    def nextRound(self):
+        self.round += 1
+        self.cleared = False
+        for k in self.headlines:
+            if self.headlines[k] > 0:
+                self.headlines[k] *= -1
+
+    def clearRound(self):
+        self.cleared = True
+
+
+    def getInfo(self):
+        return {
+            'round': self.round,
+            'likes': self.headlines,
+        }
+
+
+class User(ndb.Model):
+    team = ndb.IntegerProperty()
+    headlines = ndb.JsonProperty()
+
+
+class LoginHandler(webapp2.RequestHandler):
+    def get(self):
+        login_template = JINJA_ENV.get_template('index.html')
+        self.response.out.write(login_template.render())
+
+
+app = webapp2.WSGIApplication(
+    [
+        ('/', LoginHandler),
+    ], debug=True)
