0e131bc870b7ce85afba3439f0e97ffcf80f33c6
Nitsan Shai <shai.nitsan@gmail.com>
2017-04-18 01:50:05 -0400

    small fix to admin page

--- js/admin.js
+++ js/admin.js
@@ -35,6 +35,8 @@ $(document).ready(function() {
             contentType: false,
             processData: false,
             success: function(state) {
+                var state = JSON.parse(state);
+                $('#showInfo').empty();
                 for (var i in state['public_headlines']) {
                     $('#showInfo').append('<h4 class="col s6">'+state['public_headlines'][i]+'</h4>');
                     $('#showInfo').append('<h4 class="col s6">'+state['likes'][state['public_headlines'][i]]+'</h4>');
